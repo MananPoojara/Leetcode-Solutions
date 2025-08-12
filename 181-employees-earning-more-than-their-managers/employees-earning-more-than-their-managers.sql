844ca6e9bea18c514973ad89886bd8ad1aed5abd
Manan <104253184+MananPoojara@users.noreply.github.com>
2025-08-12 18:41:06 +0530

    Time: 389 ms (45.25%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -2,4 +2,4 @@
 -- SELECT a.name As Employee FROM Employee AS a, Employee AS b
 -- WHERE a.managerId = b.id AND a.salary > b.salary;
 
-SELECT a.name as Employee FROM Employee AS a Join Employee as b on a.managerID = b.id where a.salary > b.salary
+SELECT a.name as Employee FROM Employee AS a Join Employee as b on a.managerID = b.id where a.salary>b.salary;
